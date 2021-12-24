a9555ea2e1fadc3c04e0a28e4e09eccf36c053d1
wisuyeon <sye0n@knou.ac.kr>
2021-12-24 11:54:44 +0900

    211224 커밋

--- resource/js/script.js
+++ resource/js/script.js
@@ -178,25 +178,25 @@ function toggleContent(b,t) {
 
 $(function(){
     // F12 버튼 방지
-    // $(document).ready(function(){
-    //     $(document).bind('keydown',function(e){
-    //         if ( e.keyCode == 123 /* F12 */) {
-    //             e.preventDefault();
-    //             e.returnValue = false;
-    //         }
-    //     });
-    // });
+    $(document).ready(function(){
+        $(document).bind('keydown',function(e){
+            if ( e.keyCode == 123 /* F12 */) {
+                e.preventDefault();
+                e.returnValue = false;
+            }
+        });
+    });
 
     // 우측 클릭 방지
-    // document.onmousedown=disableclick;
-    // status="Right click is not available.";
-    //
-    // function disableclick(e){
-    //     if (e.button == 2) {
-    //         alert(status);
-    //         return false;
-    //     }
-    // }
+    document.onmousedown=disableclick;
+    status="Right click is not available.";
+
+    function disableclick(e){
+        if (e.button == 2) {
+            alert(status);
+            return false;
+        }
+    }
 
     //scroll
     var titleBar = $('#container .page-title-bar'),
